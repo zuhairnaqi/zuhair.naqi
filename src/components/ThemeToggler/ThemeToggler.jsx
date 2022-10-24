2c7d3df6db1e6eeb74046c40fddec2fc89e60b86
zuhairnaqi <zuhairnaqi@gmail.com>
2022-10-25 00:51:01 +0500

    fix: theme toggle needs to click multiple time to change theme

--- src/components/ThemeToggler/ThemeToggler.jsx
+++ src/components/ThemeToggler/ThemeToggler.jsx
@@ -1,22 +1,32 @@
 import React from "react"
 import { ThemeToggler } from "gatsby-plugin-dark-mode"
 import FormControlLabel from "@mui/material/FormControlLabel"
-import {MaterialUISwitch} from './SwitchStyled'
+import { MaterialUISwitch } from "./SwitchStyled"
 
 export default function () {
   return (
     <ThemeToggler>
-      {({ theme, toggleTheme }) => (
-        <FormControlLabel
-          control={
-            <MaterialUISwitch
-              sx={{ m: 1 }}
-              checked={theme === "dark"}
-              onChange={e => toggleTheme(e.target.checked ? "dark" : "light")}
-            />
-          }
-        />
-      )}
+      {({ theme, toggleTheme }) => {
+        // Don't render anything at compile time. Deferring rendering until we
+        // know which theme to use on the client avoids incorrect initial
+        // state being displayed.
+        if (theme == null) {
+          return null
+        }
+        return (
+          <FormControlLabel
+            control={
+              <MaterialUISwitch
+                sx={{ m: 1 }}
+                checked={theme === "dark"}
+                onChange={e => {
+                  toggleTheme(e.target.checked ? "dark" : "light")
+                }}
+              />
+            }
+          />
+        )
+      }}
     </ThemeToggler>
   )
 }
